11b9e519f7c27d9a628db616f9378755948da0b6
Haafzd <haafizd.alhabib@gmail.com>
2026-05-18 23:05:35 +0700

    Fix UI test for headless CI

--- src/test/java/com/houarizegai/calculator/CalculatorUITest.java
+++ src/test/java/com/houarizegai/calculator/CalculatorUITest.java
@@ -1,7 +1,6 @@
 package com.houarizegai.calculator;
 
 import com.houarizegai.calculator.ui.CalculatorUI;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,16 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorUITest {
 
-    private CalculatorUI calculatorUI;
-
-    @BeforeEach
-    void setUp() {
-        calculatorUI = new CalculatorUI();
-    }
-
     @ParameterizedTest
     @CsvSource({"3,5,+,8", "2,8,-,-6", "44.5,10,*,445", "320,5,/,64", "3,5,%,3", "5,3,^,125"})
     void testCalculation(double firstNumber, double secondNumber, char operator, double expectedResult) {
-        assertEquals(expectedResult, calculatorUI.calculate(firstNumber, secondNumber, operator));
+        assertEquals(expectedResult, CalculatorUI.calculate(firstNumber, secondNumber, operator));
     }
 }
